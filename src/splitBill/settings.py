c0dd48ffd01aa4a7ef248049e3006a177507ff36
Lane Rettig <rettig@wharton.upenn.edu>
2013-01-19 22:32:59 -0500

    changed paths to my paths

--- src/splitBill/settings.py
+++ src/splitBill/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/rushilg/pennapps13/splitBill/src/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -106,7 +106,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/rushilg/pennapps13/splitBill/templates',
+    #'/Users/rushilg/pennapps13/splitBill/templates',
+    '/Users/rettig/git/splitBill2/splitBill/templates',
 )
 
 INSTALLED_APPS = (
