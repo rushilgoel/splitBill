f0b53ebe7b81d7040979d635583cb451705fd1ef
Rushil Goel <rushilgoel@gmail.com>
2013-01-19 22:23:43 -0500

    form processing

--- src/billbuddy/urls.py
+++ src/billbuddy/urls.py
@@ -5,6 +5,7 @@ from billbuddy import views
 urlpatterns = patterns('',
     # ex: /polls/
     url(r'^$', views.index, name='index'),
+    url(r'^check_in/$', views.check_in, name='check_in'),
     url(r'^order/(?P<restaurant_id>\d+)/$', views.order, name='order'),
 
 )
